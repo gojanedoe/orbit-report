5caaa3bd1ab9f9ecc2582518b67f92ef2c591d8d
Jane Doe <77185646+gojanedoe@users.noreply.github.com>
2021-07-02 18:10:19 -0400

    Highlight space debris

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -12,4 +12,13 @@ export class Satellite {
         this.orbitType = orbitType;
         this.operational = operational;
     }
+
+    shouldShowWarning(): boolean {
+        //shouldShowWarning returns true if the satellite type is 'Space Debris', and it returns false otherwise. Note that this check should be case-insensitive.
+        if (this.type.toLowerCase() === 'space debris') {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
